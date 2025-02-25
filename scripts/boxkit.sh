2be784885747f7677a63579fbee470536097fdfc
larnhow <larnhow@gmail.com>
2025-02-25 12:22:42 +0100

    Update boxkit.sh

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -5,4 +5,5 @@
 
 # Update the container and install packages
 sudo zypper --non-interactive dup
+sudo zypper --non-interactive inr
 grep -v '^#' ./boxkit.packages | xargs sudo zypper --non-interactive in
