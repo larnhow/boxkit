d138e9f48a58df142d7d5702506561c1d59deb70
larnhow <larnhow@gmail.com>
2025-03-21 15:59:07 +0100

    Update boxkit.sh

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -6,4 +6,6 @@
 # Update the container and install packages
 sudo zypper --non-interactive dup
 sudo zypper --non-interactive inr
+echo 'LANG=de_DE.UTF-8' | sudo tee /etc/locale.conf
+echo 'LC_CTYPE=de_DE.UTF-8' | sudo tee -a /etc/locale.conf
 grep -v '^#' ./boxkit.packages | xargs sudo zypper --non-interactive in
